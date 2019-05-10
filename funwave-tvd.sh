23448f5522c9bfc8f76bdb67652ea451a6055cd0
ysboss <ms.ysboss@gmail.com>
2019-05-10 10:27:53 -0500

    Revise funwave installation

--- funwave-tvd.sh
+++ funwave-tvd.sh
@@ -2,15 +2,20 @@
 
 source /usr/local/bin/env.sh
 
-if [ "$Funwave-tvd_VER" != "" ]
+if [ "$Funwave_tvd_VER" != "" ]
 then
-    echo "BUILDING FUNWAVE-TVD: Version $Funwave-tvd"
+    echo "BUILDING FUNWAVE-TVD: Version $Funwave_tvd_VER"
 
-    export BASE_DIR="/workdir/$CONTAINER_VER/funwave-tvd$Funwave-tvd_VER"
+    export BASE_DIR="/workdir/$CONTAINER_VER/funwave-tvd$Funwave_tvd_VER"
     mkdir -p $BASE_DIR
     cd $BASE_DIR
-    git clone https://github.com/fengyanshi/FUNWAVE-TVD
-    cd FUNWAVE-TVD/src 
+    if [ ! -r Version_$Funwave_tvd_VER.tar.gz ] 
+    then
+        git clone https://github.com/fengyanshi/FUNWAVE-TVD/archive/Version_$Funwave_tvd_VER.tar.gz
+    fi
+    rm -fr FUNWAVE-TVD-Version_$Funwave_tvd_VER 
+    tar xvf Version_$Funwave_tvd_VER.tar.gz
+    cd FUNWAVE-TVD-Version_$Funwave_tvd_VER/src 
     perl -p -i -e 's/FLAG_8 = -DCOUPLING/#$&/' Makefile
     
     echo "BUILDING HERE:"
